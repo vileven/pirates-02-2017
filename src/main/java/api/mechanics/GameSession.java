eece0d6473f07ca3c26923e76f55187ae6f8708b
vileven <vileven11@icloud.com>
2017-06-01 23:20:47 +0300

    Added GameSession and GameUser

--- src/main/java/api/mechanics/GameSession.java
+++ src/main/java/api/mechanics/GameSession.java
@@ -1,10 +1,11 @@
 package api.mechanics;
 
+import api.mechanics.avatar.GameUser;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashSet;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -18,12 +19,68 @@ public class GameSession {
     @NotNull
     private final Long sessionId;
 
-
+    @NotNull
+    private final GameUser first;
+    @NotNull
+    private final GameUser second;
 
     private final long startTime;
 
-    public GameSession(@NotNull Long sessionId, long startTime) {
-        this.sessionId = sessionId;
+    public GameSession(@NotNull GameUser first, @NotNull GameUser second, long startTime) {
+        this.sessionId = ID_GENERATOR.getAndIncrement();;
+        this.first = first;
+        this.second = second;
         this.startTime = startTime;
+    }
+
+    @NotNull
+    public GameUser getEnemy(@NotNull GameUser user) {
+        if (Objects.equals(user, first)) {
+            return second;
+        }
+
+        if (Objects.equals(user, second)) {
+            return first;
+        }
+
+        throw new IllegalArgumentException("Requested enemy for game but user not participant");
+    }
+
+
+    @NotNull
+    public GameUser getSelf(long userId) {
+        if (first.getUser().getId().equals(userId)) {
+            return first;
+        }
+        if (second.getUser().getId().equals(userId)) {
+            return second;
+        }
+        throw new IllegalArgumentException("Request self for game but user not participate it");
+    }
+
+    @NotNull
+    public GameUser getFirst() {
+        return first;
+    }
+
+    @NotNull
+    public GameUser getSecond() {
+        return second;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GameSession that = (GameSession) o;
+
+        return sessionId.equals(that.sessionId);
+    }
+
+    @Override
+    public int hashCode() {
+        return sessionId.hashCode();
     }
 }
--- src/main/java/api/mechanics/avatar/GameUser.java
+++ src/main/java/api/mechanics/avatar/GameUser.java
@@ -1,7 +1,21 @@
 package api.mechanics.avatar;
 
+import api.model.User;
+import org.jetbrains.annotations.NotNull;
+
 /**
- * Created by Vileven on 01.06.17.
+ * Здесь класс игрока, здесь будут в полях вся информация о нем, координаты, очки, хп и т.д.
  */
 public class GameUser {
+    @NotNull
+    private final User user;
+
+    public GameUser(@NotNull User user) {
+        this.user = user;
+    }
+
+    @NotNull
+    public User getUser() {
+        return user;
+    }
 }
